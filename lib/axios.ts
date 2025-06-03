aefb2fb6fb3f2d2a949f1170a8f87493814af962
dityaww <111202012838@mhs.dinus.ac.id>
2025-06-03 20:21:07 +0700

    setting axios instance

---
+++ lib/axios.ts
@@ -0,0 +1,13 @@
+// lib/axios.ts
+import axios from 'axios';
+
+const axiosInstance = axios.create(
+  {
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    // withCredentials: true
+  });
+
+export default axiosInstance;
